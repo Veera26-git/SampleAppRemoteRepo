2411474ce4811a6c444b9da8dbee004a9af53067
Veera26-git <veera.gitams26@gmail.com>
2021-05-05 16:18:47 +0530

    Initial Commit

---
+++ DevOpsDemo/src/test/java/com/cg/DevOpsDemo/SampleTest.java
@@ -0,0 +1,24 @@
+package com.cg.DevOpsDemo;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SampleTest {
+	
+    @Test
+    public void test1() {
+        Assert.fail();
+    }
+    @Test
+    public void test2() {
+        Assert.fail();
+    }
+    @Test
+    public void test3() {
+       
+    }
+    @Test
+    public void test4() {
+    }
+
+}
